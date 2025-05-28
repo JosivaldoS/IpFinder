c54266d27c83d396b1975e5b0bf94b2640b53d1c
Josivaldo Soares <77576339+JosivaldoS@users.noreply.github.com>
2025-05-28 20:21:11 -0300

    Adicionando todo o conteúdo do meu site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import InputProcurar from './components/inputProcurar/inputProcurar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>
+        Ip Finder
+        <InputProcurar/>
+      </h1>
+    </main>
   );
 }
 
---
+++ src/components/inputProcurar/inputProcurar.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react"
+import React from "react"
+
+export default function InputProcurar(){
+
+
+    let enderecoIp = 9
+
+    /*}
+
+    useEffect(() => {
+        fetch('https://geo.ipify.org/api/v2/country,city?apiKey=at_JH7C5Zg0ven1hiDeuPc6DUQCrgVeE&ipAddress=29.212.190.192')
+            .then(response => response.json())
+            .then(data => console.log(data.location))
+            // Caso eu queira puxar algum valor, por exemplo a cidade é só mudar o data.location para data.location.city
+            .catch(err => console.error(err))
+    }, [])
+    */
+
+    return (
+        <>
+            <input></input>
+        </>
+    )
+}
